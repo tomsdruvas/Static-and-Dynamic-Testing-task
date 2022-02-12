ef4cb7dc34242082dd03576d7d0e97db3239de16
Toms Druvaskalns <tomsdruvas@gmail.com>
2022-02-12 20:43:07 +0000

    completed all tests

--- part_1_docs/testing_task_1.md
+++ part_1_docs/testing_task_1.md
@@ -19,21 +19,34 @@ class CardGame:
 
   def check_for_ace(self, card):
     if card.value = 1:
+      # should be == and not =
       return True
     else
+    #need to add a colon ":"
       return False
+
+
+# 1)if card.value = 1 needs to be double equals(==)
+# 2) there needs to be a : after else   
    
 
   dif highest_card(self, card1 card2):
+  # dif should be def
   if card1.value > card2.value:
     return card
   else:
     return card2
+
+# 1)instead of dif there needs to be def
+# 2)the indentation between first and second liune wasnt right
+# 3) comma after the card1 parameter is missing
+# 4) in the first return statement needs to be card1 as that is the parameter mentioned in the first line
   
 
 
 def cards_total(self, cards):
   total
+  # total should equal something, most liekly 0
   for card in cards:
     total += card.value
     return "You have a total of" + total
---
+++ part_2_code/tests/card_game_tests.py
@@ -0,0 +1,44 @@
+import unittest
+from unittest import result
+from src.card import Card
+from src.card_game import CardGame
+
+class TestCardGame(unittest.TestCase):
+    def setUp(self):
+        self.card1 = Card("clubs", 3)
+        self.card2 = Card("diamonds", 2)
+        self.card3 = Card("hearts", 1)
+        self.cardgame = CardGame()
+        self.cards = [self.card1, self.card2, self.card3]
+
+
+    def test_card_has_suit(self):
+        self.assertEqual("clubs", self.card1.suit)
+    
+    def test_card_has_value(self):
+        self.assertEqual(2, self.card2.value)
+
+    def test_check_for_ace_True(self):
+        result = True
+        expected = self.cardgame.check_for_ace(self.card3)
+        self.assertEqual(result, expected)
+
+    def test_check_for_ace_False(self):
+        result = False
+        expected = self.cardgame.check_for_ace(self.card2)
+        self.assertEqual(result, expected)
+
+    def test_highest_card(self):
+        result = self.card1
+        expected = self.cardgame.highest_card(self.card1, self.card2)
+        self.assertEqual(result, expected)
+
+    def test_cards_total(self):
+        result = "You have a total of 6"
+        expected = self.cardgame.cards_total(self.cards)
+        self.assertEqual(result, expected)
+    
+
+    
+
+    
